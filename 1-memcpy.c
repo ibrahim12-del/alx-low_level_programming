ce3ce19a91fe1ac579fae35237368ac7c90b04e3
ibrahim12_del <specialhealthcare1234@gmail.com>
2023-08-30 03:29:19 -0700

    lk

--- 1-memcpy.c
+++ 1-memcpy.c
@@ -8,14 +8,15 @@
  *Return: copied memory with n bytes changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
-{
+	
 	int r = 0;
 	int i = n;
 
 	for (; r < i; r++)
 	{
-		dest = src[r];
+		dest[r] = src[r];
 		n--;
+
 	}
 	return (dest);
 }
