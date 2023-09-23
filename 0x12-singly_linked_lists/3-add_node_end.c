5a3eeb98d37f19d483b71452e01d5a174842b281
ibrahim12-del <specialhealthcare1234@gmail.com>
2023-09-22 19:30:01 -0700

    lk

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * add_node_end - at the end of a new node add linked list
@@ -16,14 +17,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *temp = *head;
 	unsigned int len = 0;
 	
-	while (str[len]
+	while(str[len])
 		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
-	new->str = strdup(str)
+	new->str = strdup(str);
 	new->len = len;
 	new->next = NULL;
 
@@ -35,7 +36,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	
 	while (temp->next)
-		temp= temp->next
+		temp= temp->next;
 
 	temp->next = new;
 
