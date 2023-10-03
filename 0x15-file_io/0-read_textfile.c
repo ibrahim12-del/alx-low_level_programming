307062d418cd52d0d3f3f99e2fd856ecc022905a
ibrahim12-del <specialhealthcare1234@gmail.com>
2023-10-03 14:58:02 -0700

    lkj

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <atdlib.h>
+#include <stdlib.h>
 
 /**
  * read_textfile- Read text file print to STDOUT
  * @filename: text file being read
  * @lettes: number of letters to be read 
  * Return: w- actual number of bytes read and printed
- *	0	when duntion fails or filename is NULL.
+ *	0 when duntion fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if(fd == -1)
-		return (0)
+		return (0);
 	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
